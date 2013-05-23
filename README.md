4251c9a16c0d9da999a4407b4a29d35e88e41895
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-05-23 13:45:51 +0200

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ colorpicker
 ===========
 
 A color picker that allows the user to choose from a set of predefined colors
+
+
+== Dependencies ==
+
+jQuery
+colorpicker.css
+colorpicker.js
+
+== Usage ==
+
+Include the css and javascript file and initialize the input field you want to use for colorpicking
+
+  		$('.colorpicker').colorpicker();
---
+++ colorpicker.js
@@ -0,0 +1,55 @@
+/*
+  Licensed "AS IF" by Fabien Tesselaar (Thursday may 23, 2013)
+  https://github.com/Tessmore/colorpicker
+*/
+
+$.fn.colorpicker = function() {
+
+  var picker = $('<ul id="colorpicker" />');
+  
+  var colors = {
+    'light-green' : '#d8f3b1',
+    'green'       : '#94deaf',
+    'dark-green'  : '#67d08d',
+    'light-blue'  : '#a6f8ff',
+    'blue'        : '#87c3eb',
+    'yellow'      : '#ffffb1',
+    'orange'      : '#ffd78b',
+    'red'         : '#f28f8b',
+    'light-brown' : '#f1ebbb',
+    'brown'       : '#d0c9a4'
+  };
+    
+  for (var color in colors)
+    picker.append('<li style="background:'+ colors[color] +'" title="'+ color +'">'+ colors[color] +'</li>');
+  
+  $('body').append(picker);
+  
+  return this.each(function() {
+    
+    var input = $(this);
+    
+    input.on('focus, click', function() {
+      var offset = $(this).offset();
+      
+      picker.css({
+        'display' : 'block',
+        'top'  : offset.top  + 34,
+        'left' : offset.left - 27,
+      })
+      .children().click(function() {        
+        input
+          .val($(this).text())
+          .css('background', $(this).text());
+          
+        input.focus();
+      });
+    });
+    
+    input.blur(function() {
+      if (! picker.is(':hover')) 
+        picker.fadeOut(200);
+    });
+    
+  });
+};
